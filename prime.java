661603f9c28a6e82a7fcbdf1ad1518af27b803da
sushmitha <sushmithachitneni@gmail.com>
2024-11-22 23:43:18 +0530

    ajj

--- prime.java
+++ prime.java
@@ -1,9 +1,9 @@
-ublic class Prime
+public class Prime
 {
     public static void main(String[] args)
     {
         int num=3489;
-        boolean isPrime=truel
+        boolean isPrime=true;
         for(int i=2;i<=num/2;i++)
         {
             if(num%i==0)
